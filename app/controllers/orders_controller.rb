82101ec20bfd81bd364d314538ae7cbafa84b363
Ruslan Zekokh <zekokh@icloud.com>
2017-04-12 14:10:31 +0300

    Initial commit

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+  def advarts
+
+  end
+
+  def date
+
+  end
+
+  def theme
+
+  end
+
+  def shape
+
+  end
+
+  def floor
+
+  end
+end
---
+++ app/service/order.rb
@@ -0,0 +1,3 @@
+class Order
+  attr_accessor :name_of_client, :id_of_client
+end
